d8c64e26179f27492b3a3ce77a1bc3c0cdac8dcb
Ivan <ivan-kozlow04@mail.ru>
2022-12-07 16:52:16 +0300

    update spoilers interactive

--- build/script/script.js
+++ build/script/script.js
@@ -2,16 +2,26 @@ const spoilerSection = document.querySelector(".spoiler");
 // СПОЙЛЕРЫ
 spoilerSection.addEventListener("click", function (event) {
 	// При клике открывать спойлер
+
 	if (spoilerBtn = event.target.closest('.spoiler__button')) {
 		spoilerBtn.parentNode.classList.toggle('active');
+		arrSpolers = spoilerSection.querySelectorAll('.active')
+
+		if (arrSpolers.length > 1) {
+			arrSpolers.forEach(el => {
+				el.classList.remove('active');
+			});
+			spoilerBtn.parentNode.classList.add('active');
+		}
 
 	} else if (spoilerText = event.target.closest('.spoiler__text')) {
 		spoilerText.parentNode.classList.add('active');
+		console.log('click on text');
 		// Закрыть спойлер (Всем элементам массива убирается класс active)
 	} else {
-		arrSpolers = spoilerSection.querySelectorAll('.spoiler__item')
 		arrSpolers.forEach(el => {
 			el.classList.remove('active');
 		});
+		console.log('clear classes');
 	}
 });
